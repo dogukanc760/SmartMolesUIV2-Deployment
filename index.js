8d287e0a65fcd4bb395682fc3c44240894107596
CoderBig <49120922+dogukanc760@users.noreply.github.com>
2022-08-07 19:38:52 +0300

    mred

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("server running at " + 3000);
+app.listen(process.env.PORT, () => {
+  console.log("server running at " + process.env.PORT);
 });
