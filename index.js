a315819152742b81884c6db255bcb4bb080e4599
CoderBig <49120922+dogukanc760@users.noreply.github.com>
2022-08-07 19:40:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './build', 'index.html'));
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("server running at " + process.env.PORT);
 });
